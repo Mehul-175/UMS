5727ba8995f002c8c6a286c057fef48401f619a1
Mehul-175 <mehul110359@gmail.com>
2025-12-29 12:18:28 +0530

    initial project setup

---
+++ server/src/index.js
@@ -0,0 +1,24 @@
+import './configEnv.js';
+import express from 'express';
+import cookieParser from 'cookie-parser'
+import cors from 'cors';
+import { connectDB } from './utils/db.js';
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+
+//Middlewares
+app.use(cors({
+    origin: "http://localhost:5174",
+    credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
+
+//Routes
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port: ${PORT}`)
+    connectDB();
+})
