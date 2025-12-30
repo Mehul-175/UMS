a09d58689ac64868664b66c75d923b418d9ec819
Mehul-175 <mehul110359@gmail.com>
2025-12-30 09:16:48 +0530

    fix: add auth protection to logout route

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { login, logout, register } from '../controllers/auth.controllers.js';
+import { protectedRoutes } from '../middlewares/auth.middleware.js';
 
 const Router = express.Router();
 
 Router.post('/register', register);
 Router.post('/login', login)
-Router.post('/logout', logout)
+Router.post('/logout', protectedRoutes, logout)
 
 export default Router
